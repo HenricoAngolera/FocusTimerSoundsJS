8e641c9d8c14394213de7cf2fc9e83d3807048ef
henrico <henrico.an@gmail.com>
2023-02-23 16:15:13 -0300

    fix sounds

--- js/sounds.js
+++ js/sounds.js
@@ -1,11 +1,15 @@
 export default function () {
-  const forest = new Audio('../audio/Floresta.wav')
-  const rain = new Audio('../audio/Chuva.wav')
-  const coffee = new Audio('../audio/Cafeteria.wav')
-  const fire = new Audio('../audio/Lareira.wav')
+  const forest = new Audio('./audio/Floresta.wav')
+  const rain = new Audio('./audio/Chuva.wav')
+  const coffee = new Audio('./audio/Cafeteria.wav')
+  const fire = new Audio('./audio/Lareira.wav')
 
-  const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
-  const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
+  const buttonPressAudio = new Audio(
+    'https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true'
+  )
+  const kitchenTimer = new Audio(
+    'https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true'
+  )
 
   forest.loop = true
   rain.loop = true
